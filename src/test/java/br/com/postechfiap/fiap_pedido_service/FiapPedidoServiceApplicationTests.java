b1f44d6a7771788d346b412f2349d01896427c87
GiordanoFerreira <giordanopalmezano@gmail.com>
2025-04-16 03:28:14 -0300

    first commit

---
+++ src/test/java/br/com/postechfiap/fiap_pedido_service/FiapPedidoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postechfiap.fiap_pedido_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapPedidoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
